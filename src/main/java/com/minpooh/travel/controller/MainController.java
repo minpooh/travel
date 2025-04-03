6e1f37f6e826dcbff080dbf00a79fa48b0b46d10
minpooh <minjoung1510@gmail.com>
2025-04-04 01:08:02 +0900

    update all

--- src/main/java/com/minpooh/travel/controller/MainController.java
+++ src/main/java/com/minpooh/travel/controller/MainController.java
@@ -6,7 +6,59 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
     @GetMapping(value = "")
-    public String main() {
-        return "main";
+    public String index() {
+        return "index";
     }
+
+    @GetMapping(value = "/travel")
+    public String travel() {
+        return "travel";
+    }
+
+    @GetMapping(value = "/travel/view")
+    public String travelView() {
+        return "travel_view";
+    }
+
+    @GetMapping(value = "/food")
+    public String food() {
+        return "food";
+    }
+
+    @GetMapping(value = "/food/view")
+    public String foodView() {
+        return "food_view";
+    }
+
+    @GetMapping(value = "/experience")
+    public String experience() {
+        return "experience";
+    }
+
+    @GetMapping(value = "/photospot")
+    public String photospot() {
+        return "photospot";
+    }
+
+    @GetMapping(value = "/mystory")
+    public String mystory() {
+        return "mystory";
+    }
+
+    @GetMapping(value = "/mystory/write")
+    public String mystoryWrite() {
+        return "mystory_write";
+    }
+
+    @GetMapping(value = "/mystory/view")
+    public String mystoryView() {
+        return "mystory_view";
+    }
+
+    @GetMapping(value = "/mystory/edit")
+    public String mystoryEdit() {
+        return "mystory_edit";
+    }
+
 }
+
